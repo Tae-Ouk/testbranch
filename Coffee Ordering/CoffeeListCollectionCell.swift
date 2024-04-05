2d2d8fd48623d15a8cd904fdecb08c17bdf645ec
Tae-Ouk <to99820@naver.com>
2024-04-05 15:13:09 +0900

    first commit

---
+++ Coffee Ordering/CoffeeListCollectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  CoffeeListCollectionCell.swift
+//  Coffee Ordering
+//
+//  Created by TaeOuk Hwang on 4/1/24.
+//
+
+import UIKit
+
+class CoffeeListCollectionCell: UICollectionViewCell {
+    
+}
